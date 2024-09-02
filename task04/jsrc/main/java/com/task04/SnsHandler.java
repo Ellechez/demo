5bda650f63b3bebd8fb551b236df72e537a5f691
thapa <t.hapanovich@gmail.com>
2024-09-02 16:01:15 +0200

    solution 1

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -3,13 +3,15 @@ package com.task04;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent;
 import com.syndicate.deployment.annotations.events.SnsEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.resources.DependsOn;
 import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
-import java.util.HashMap;
-import java.util.Map;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @LambdaHandler(
 		lambdaName = "sns_handler",
@@ -24,16 +26,21 @@ import java.util.Map;
 		name = "lambda_topic",
 		resourceType = ResourceType.SNS_TOPIC
 )
-public class SnsHandler implements RequestHandler<Object, Map<String, Object>> {
-	public SnsHandler() {
-	}
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
+
+public class SnsHandler implements RequestHandler<SNSEvent, List<String>> {
+
+	@Override
+	public List<String> handleRequest(SNSEvent snsEvent, Context context) {
 		LambdaLogger logger = context.getLogger();
-		logger.log(request.toString());
-		Map<String, Object> resultMap = new HashMap();
-		resultMap.put("statusCode", 200);
-		resultMap.put("message", "Hello from Lambda");
-		return resultMap;
+		logger.log("EVENT TYPE" + snsEvent.getClass().toString());
+		List<String> msg = new ArrayList<>();
+
+		for (SNSEvent.SNSRecord m : snsEvent.getRecords()) {
+			logger.log("sns message " + m.getSNS().getMessage());
+			msg.add(m.getSNS().getMessage());
+		}
+		return msg;
+
 	}
 }
--- task04/jsrc/main/java/com/task04/SqsHandler.java
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -3,13 +3,15 @@ package com.task04;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.resources.DependsOn;
 import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
-import java.util.HashMap;
-import java.util.Map;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @LambdaHandler(
 		lambdaName = "sqs_handler",
@@ -25,16 +27,18 @@ import java.util.Map;
 		name = "async_queue",
 		resourceType = ResourceType.SQS_QUEUE
 )
-public class SqsHandler implements RequestHandler<Object, Map<String, Object>> {
-	public SqsHandler() {
-	}
+public class SqsHandler implements RequestHandler<SQSEvent, List<String>> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
+	@Override
+	public List<String> handleRequest(SQSEvent sqsEvent, Context context) {
 		LambdaLogger logger = context.getLogger();
-		logger.log(request.toString());
-		Map<String, Object> resultMap = new HashMap();
-		resultMap.put("statusCode", 200);
-		resultMap.put("message", "Hello from Lambda");
-		return resultMap;
+		logger.log("EVENT TYPE" + sqsEvent.getClass().toString());
+		List<String> msg = new ArrayList<>();
+
+		for (SQSEvent.SQSMessage m : sqsEvent.getRecords()) {
+			logger.log("sqs message " + m.getBody());
+			msg.add(m.getBody());
+		}
+		return msg;
 	}
 }
