591d4a015fd40b48548b83c5f572c5c3489ff7b3
thapa <t.hapanovich@gmail.com>
2024-09-09 12:56:12 +0200

    solution 1.2

--- task08/jsrc/main/java/com/task08/ApiHandler.java
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -1,12 +1,12 @@
 package com.task08;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
-import com.epam.openapi.OpenAPISDK;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaLayer;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.Architecture;
 import com.syndicate.deployment.model.ArtifactExtension;
 import com.syndicate.deployment.model.DeploymentRuntime;
 import com.syndicate.deployment.model.RetentionSetting;
@@ -14,54 +14,33 @@ import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
 import java.util.Map;
-import java.util.function.Function;
 
-@LambdaHandler(lambdaName = "api_handler",
+@LambdaHandler(
+		lambdaName = "api_handler",
 		roleName = "api_handler-role",
+		layers = {"openmetro-layer"},
 		isPublishVersion = false,
-		runtime = DeploymentRuntime.JAVA11,
-		layers = {"open-apisdk-layer"},
 		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 @LambdaLayer(
-		layerName = "open-apisdk-layer",
+		layerName = "openmetro-layer",
+		libraries = {"lib/task08-1.0.0.jar"},
 		runtime = DeploymentRuntime.JAVA11,
-		libraries = {"lib/open-meteo-1.0.jar"},
+		architectures = {Architecture.ARM64},
 		artifactExtension = ArtifactExtension.ZIP
 )
 @LambdaUrlConfig(
 		authType = AuthType.NONE,
 		invokeMode = InvokeMode.BUFFERED
 )
-public class ApiHandler implements RequestHandler<APIGatewayV2HTTPEvent, String> {
-	private final Map<Key, Function<APIGatewayV2HTTPEvent, String>> routeHandlers =
-			Map.of(new Key("GET", "/weather"), this::getForecast);
-	private Key key;
-
-	@Override
-	public String handleRequest(APIGatewayV2HTTPEvent requestEvent, Context context) {
-		key = new Key(getMethod(requestEvent), getPath(requestEvent));
-		return routeHandlers.getOrDefault(key, this::errorResp).apply(requestEvent);
+public class ApiHandler implements RequestHandler<Map<String, Object>, String> {
+	public String handleRequest(Map<String, Object> request, Context context) {
+		OpenMetroClient client = new OpenMetroClient();
+		try {
+			String weatherForecast = client.getWeatherForecast();
+			return weatherForecast;
+		} catch (Exception e) {
+			return e.getMessage();
+		}
 	}
-
-	private String errorResp(APIGatewayV2HTTPEvent apiGatewayV2HTTPEvent) {
-		return "{" +
-				"\"statusCode\": 404," +
-				"\"message\": \"Bad request syntax or unsupported method\"" +
-				"}";
-	}
-
-	private String getMethod(APIGatewayV2HTTPEvent requestEvent) {
-		return requestEvent.getRequestContext().getHttp().getMethod();
-	}
-
-	private String getPath(APIGatewayV2HTTPEvent requestEvent) {
-		return requestEvent.getRequestContext().getHttp().getPath();
-	}
-
-	private String getForecast(APIGatewayV2HTTPEvent apiGatewayV2HTTPEvent) {
-		OpenAPISDK openapi = new OpenAPISDK();
-		return openapi.getForecast();
-	}
-
 }
--- task08/jsrc/main/java/com/task08/Key.java
+++
@@ -1,8 +0,0 @@
-package com.task08;
-
-public class Key {
-    private String method;
-    private String path;
-    public Key(String method, String path) {
-    }
-}
